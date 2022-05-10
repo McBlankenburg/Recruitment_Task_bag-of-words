364bb7ff5ba7c735169822ffc04f3a9209c93bf2
mcblankenburg <maciej.blankenburg@gmail.com>
2022-05-11 00:03:23 +0200

    refactoring

--- src/main/java/com/tomtom/lns/interview/BagOfWords.java
+++ src/main/java/com/tomtom/lns/interview/BagOfWords.java
@@ -1,40 +1,31 @@
 package com.tomtom.lns.interview;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Locale;
 
 class BagOfWords {
 
-    private String document;
-    private HashMap hashMap = new HashMap<String, Integer>();
+    final private HashMap hashMap = new HashMap<String, Long>();
 
     public BagOfWords(String document) {
-        this.document = document;
+        String [] words = document.split(" ");
+        for (String word : words){
+            String key = word.toLowerCase();
+
+            if(hashMap.containsKey(key)){
+                int value = 1 + (int) hashMap.get(key);
+                hashMap.put(key, value);
+            }else{
+                hashMap.put(key, 1);
+            }
+        }
     }
 
     long size() {
-        String [] words = document.split(" ");
-         for (String word : words){
-            String key = word.toLowerCase();
-
-             if(hashMap.containsKey(key)){
-                 int value = 1 + (int) hashMap.get(key);
-                 hashMap.put(key, value);
-             }else{
-                 hashMap.put(key, 1);
-             }
-         }
         return hashMap.size();
     }
 
     long tokenFrequency(String token) {
-        size();
         String key = token.toLowerCase();
-        int valueOfKey = 0;
-        if(hashMap.containsKey(key)){
-           valueOfKey = (int) hashMap.get(key);
-        }
-        return valueOfKey;
+        return Long.parseLong(hashMap.get(key).toString());
     }
 }
